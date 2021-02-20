653fd1a96dbdaac6a074e5b942e1d72d73161a6f
muneobu <muneobu@gmail.com>
2021-02-20 22:48:17 +0900

    :fish:

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let a = 10;
-let b = a / 0;
-a = 1;
-console.log(b, a);
-let c = Math.random() * 10;
-console.log(Math.floor(c));
+const array = ["ubu", "muneo"];
+const oobject = {
+  human: "ubu",
+  fish: "muneo",
+};
+
+const family = {
+  human: "umma",
+  fish: "appa",
+};
+
+function sayHello(octopus, fisherman) {
+  console.log("Hello!", octopus, "are you", fisherman, "?");
+}
+
+function sayHi(aarray) {
+  console.log("Hi!", aarray[1], "Are you", aarray[0], "?");
+}
+
+function sayyo(ooobject) {
+  console.log("Yo you caught", ooobject.fish, "are you", ooobject.human, "?");
+}
+sayHello("문어", "어부");
+sayHi(array);
+sayyo(oobject);
+sayyo(family);
