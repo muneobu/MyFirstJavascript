ff1606961c087ac5c8860c5d007512bda3f08383
muneobu <muneobu@gmail.com>
2021-02-22 22:00:08 +0900

    :mask:

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-const array = ["ubu", "muneo"];
-const oobject = {
-  human: "ubu",
-  fish: "muneo",
-};
-
-const family = {
-  human: "umma",
-  fish: "appa",
-};
-
-function sayHello(octopus, fisherman) {
-  console.log("Hello!", octopus, "are you", fisherman, "?");
-}
-
-function sayHi(aarray) {
-  console.log("Hi!", aarray[1], "Are you", aarray[0], "?");
-}
-
-function sayyo(ooobject) {
-  console.log("Yo you caught", ooobject.fish, "are you", ooobject.human, "?");
-}
-sayHello("문어", "어부");
-sayHi(array);
-sayyo(oobject);
-sayyo(family);
+const title = document.getElementById("title");
+title.innerText = "Muneobu";
+console.log(title);
+console.error("뽝");
