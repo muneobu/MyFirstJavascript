d0aa46cdc0c886585e077d6608a315857f9fcc3f
muneobu <muneobu@gmail.com>
2021-03-01 20:39:21 +0900

    :sunny:

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const title = document.getElementById("title");
+const title = document.querySelector("#title");
 title.innerText = "Muneobu";
-console.log(title);
+console.dir(title);
 console.error("뽝");
+title.style.color = "blue";
+console.dir(document);
+document.title = "eobuscript";
