3b76049fdb3a82da781e54c99095ee0e92b2ff6b
muneobu <muneobu@gmail.com>
2021-03-06 21:21:10 +0900

    :happy:

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const title = document.querySelector("#title");
-title.innerText = "Muneobu";
-console.dir(title);
-console.error("뽝");
-title.style.color = "blue";
-console.dir(document);
-document.title = "eobuscript";
+
+const muneo = {
+  handleResize: () => {
+    console.log("I have been resized");
+  },
+};
+
+function handleResize(event) {
+  if (event.type === "click") {
+    console.log("mouse");
+    title.innerHTML = "mouse";
+    title.style.color = "red";
+  } else if (event.type === "resize") {
+    console.log("resize");
+    title.innerHTML = "resize";
+    title.style.color = "blue";
+  } else {
+    console.log("nothing");
+  }
+}
+
+let a = 2;
+let b = 3;
+const ccc = (a = 1, b = 1) => {
+  console.log(a + b);
+  return a + b;
+};
+title.innerHTML = ccc(a, b);
+
+window.addEventListener("click", handleResize);
+window.addEventListener("resize", handleResize);
