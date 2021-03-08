1228a3c866de1055ae26bcc35bc8e77a6c11c1d0
muneobu <muneobu@gmail.com>
2021-03-08 21:52:24 +0900

    :blue_heart:

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,41 @@
 const title = document.querySelector("#title");
-let age = prompt("몇살이니?");
-if (age >= 7 && age <= 19) {
-  title.innerHTML = "you are a student";
-} else if (age > 19) {
-  title.innerHTML = "you are not a student";
-} else {
-  title.innerHTML = "you are too young to go to school";
+const body = document.querySelector(".lala");
+const BASE_COLOR = "tomato";
+const OTHER_COLOR = "wheat";
+const HOVER_COLOR = "white";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+    body.style.backgroundColor = BASE_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+    body.style.backgroundColor = OTHER_COLOR;
+  }
 }
+
+function handleMouse() {
+  title.style.color = HOVER_COLOR;
+}
+
+function handleOut() {
+  const currentColor = title.style.color;
+  const ccurrentColor = body.style.backgroundColor;
+  if (currentColor === HOVER_COLOR && ccurrentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+    body.style.backgroundColor = BASE_COLOR;
+  } else if (currentColor === HOVER_COLOR && ccurrentColor === OTHER_COLOR) {
+    title.style.color = BASE_COLOR;
+    body.style.backgroundColor = OTHER_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  body.style.backgroundColor = OTHER_COLOR;
+  title.addEventListener("click", handleClick);
+  title.addEventListener("mouseenter", handleMouse);
+  title.addEventListener("mouseout", handleOut);
+}
+init();
