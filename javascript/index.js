c86c6f29c8f2578ac35a76bf544140cb6d54d8e5
muneobu <muneobu@gmail.com>
2021-03-09 22:24:21 +0900

    :sunglasses:

--- javascript/index.js
+++ javascript/index.js
@@ -1,41 +1,11 @@
 const title = document.querySelector("#title");
-const body = document.querySelector(".lala");
-const BASE_COLOR = "tomato";
-const OTHER_COLOR = "wheat";
-const HOVER_COLOR = "white";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-    body.style.backgroundColor = BASE_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-    body.style.backgroundColor = OTHER_COLOR;
-  }
-}
-
-function handleMouse() {
-  title.style.color = HOVER_COLOR;
-}
-
-function handleOut() {
-  const currentColor = title.style.color;
-  const ccurrentColor = body.style.backgroundColor;
-  if (currentColor === HOVER_COLOR && ccurrentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-    body.style.backgroundColor = BASE_COLOR;
-  } else if (currentColor === HOVER_COLOR && ccurrentColor === OTHER_COLOR) {
-    title.style.color = BASE_COLOR;
-    body.style.backgroundColor = OTHER_COLOR;
-  }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
-  body.style.backgroundColor = OTHER_COLOR;
   title.addEventListener("click", handleClick);
-  title.addEventListener("mouseenter", handleMouse);
-  title.addEventListener("mouseout", handleOut);
 }
 init();
