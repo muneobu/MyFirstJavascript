f5a0edf398be576f878b73c050534862cca3998e
muneobu <muneobu@gmail.com>
2021-03-11 22:30:52 +0900

    :stuck_out_tongue:

--- javascript/clock.js
+++ javascript/clock.js
@@ -6,12 +6,13 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  const millisecond = date.getUTCMilliseconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}:${millisecond}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
   getTime();
 }
 
-setInterval(init, 1);
+setInterval(init, 1000);
