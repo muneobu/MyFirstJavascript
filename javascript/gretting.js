647ac624003fe88a58a46fabb2ee671a8fcfa70a
muneobu <muneobu@gmail.com>
2021-03-14 20:44:32 +0900

    LocalStoragy:yum:

--- javascript/gretting.js
+++ javascript/gretting.js
@@ -4,18 +4,36 @@ const USER_LS = "username";
 const greeting = document.querySelector(".js-greetings");
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  console.log(event);
+  saveName(currentValue);
+  paintgreeting(localStorage.getItem(USER_LS));
+  input.value = null;
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML = `이름을 넣으시오`;
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintgreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innertext = `Hello ${text}`;
+  greeting.innerText = `Hello ${text}`;
 }
 
 function init() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    form.classList.add(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `이름을 넣으시오`;
+    askForName();
   } else {
     paintgreeting(currentUser);
   }
